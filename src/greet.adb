92a8ca8a6983dfde11647de9908290504632c698
Melvil <hbmelvil@gmail.com>
2026-07-20 16:54:13 +0200

    test

---
+++ src/greet.adb
@@ -0,0 +1,7 @@
+with Ada.Text.IO;
+
+procedure Greet is
+begin
+	-- This program is not related to the repostory. It's a technical test. It prints "Hello, World !" to the screen.
+	Ada.Text.IO.Put_Line ("Hello, World !");
+end Greet;
