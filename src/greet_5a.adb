1f9ce0f578ad8ae76ec202073ab97e9a39f1f623
Melvil <hbmelvil@gmail.com>
2026-07-26 14:58:00 +0200

    daily ada exercice : for loop does not execute ; upper and lower bound conflict

--- src/greet_5a.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Greet_5a_Reverse is
-begin
-    for I in reverse 1 .. 5 loop
-        Put_Line ("Hello, World!"
-                  & Integer'Image (I));
-    end loop;
-end Greet_5a_Reverse;
---
+++ src/greet_no_op.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+-- This program does not return anything while being executed, because the value of the for loop upper bound is less than the lower bound. By that, the loop is not executed at all.
+
+procedure Greet_No_Op is
+begin
+    for I in reverse 5 .. 1 loop
+        Put_Line ("Hello, World!"
+                  & Integer'Image (I));
+    end loop;
+end Greet_No_Op;
