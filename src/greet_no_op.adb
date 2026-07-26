e91e14c7191d44398f39b37a8fc0ed04400b446d
Melvil <hbmelvil@gmail.com>
2026-07-26 15:02:10 +0200

    daily ada exercice comment modification

--- src/greet_no_op.adb
+++ src/greet_no_op.adb
@@ -3,7 +3,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Greet_No_Op is
 begin
-    for I in reverse 5 .. 1 loop
+    for I in reverse 5 .. 1 loop -- Here's the conflict that causes the dysfunction of the loop.
         Put_Line ("Hello, World!"
                   & Integer'Image (I));
     end loop;
