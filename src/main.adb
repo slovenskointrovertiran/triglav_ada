0a7a262cbaed30115a376750282006825f59c34a
Melvil <hbmelvil@gmail.com>
2026-08-02 10:38:24 +0200

    daily ada exercice : case expression

--- src/main.adb
+++ src/main.adb
@@ -4,7 +4,9 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
 begin
 	for I in 1 .. 10 loop
-		Put_Line (if I mod 2 = 0 then "Even" else "Odd");
-
+		Put_Line
+		(case I is
+		when 1 | 3 | 5 | 7 | 9 => "Odd",
+		when 2 | 4 | 6 | 8 | 10 => "Even");
 	end loop;
 end Main;
