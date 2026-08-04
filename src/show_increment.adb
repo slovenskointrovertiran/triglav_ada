4e7d199ce7ed276dd5b1cb3e15fd748f0b91c21c
Melvil <hbmelvil@gmail.com>
2026-08-04 10:48:45 +0200

    daily ada exercice : nested subprograms

--- src/show_increment.adb
+++ src/show_increment.adb
@@ -3,22 +3,19 @@ with Increment_By;
 
 procedure Show_Increment is
 	A, B, C : Integer;
-begin
-	C := Increment_By;
-	-- Parameterless call, value of I is 0 and Incr is 1
-	Put_Line ("Using defaults for Increment_By is" & Integer'Image (C));
 
+	procedure Display_Result is
+	begin
+		Put_Line ("Increment of " & Integer'Image (A) & " with " & Integer'Image (B) & " is " & Integer'Image (C));
+	end Display_Result;
+
+begin
 	A := 10;
 	B := 3;
 	C := Increment_By (A, B);
-	--                 ^ Regular parameter passing
-	
-	Put_Line("Increment of " & Integer'Image (A) & " with " & Integer'Image (B) & " is " & Integer'Image (C));
-
+	Display_Result;
 	A := 20;
 	B := 5;
-	C := Increment_By ( I => A, Incr => B);
-	--                          ^ Named parameter passing
-	
-	Put_Line("Increment of " & Integer'Image (A) & " with " & Integer'Image (B) & " is " & Integer'Image (C));
+	C := Increment_By (A, B);
+	Display_Result;
 end Show_Increment;
