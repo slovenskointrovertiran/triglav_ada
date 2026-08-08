1be44d7b76359f20a6c269fda499fa36ecc552be
Melvil <hbmelvil@gmail.com>
2026-08-08 11:06:51 +0200

    daily ada exercice : renaming

---
+++ src/a_procedure_with_very_long_name_that_cannot_be_changed.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure A_Procedure_With_Very_Long_Name_That_Cannot_Be_Changed
+	(A_Message : String) is
+begin
+	Put_Line (A_Message);
+end A_Procedure_With_Very_Long_Name_That_Cannot_Be_Changed;
---
+++ src/a_procedure_with_very_long_name_that_cannot_be_changed.ads
@@ -0,0 +1,2 @@
+procedure A_Procedure_With_Very_Long_Name_That_Cannot_Be_Changed
+	(A_Message : String);
