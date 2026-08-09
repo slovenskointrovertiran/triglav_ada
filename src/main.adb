36e5c31ec2d2241f0c227dfb62ece2acd19d3e52
Melvil <hbmelvil@gmail.com>
2026-08-09 21:52:01 +0200

    daily ada exercice : package body

--- src/main.adb
+++
@@ -1,8 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Week;
-
-procedure Main is
-	use Week; -- Makes every entity of the week package directly visible here.
-begin
-	Put_Line ("First day of the week is" & Mon);
-end Main;
