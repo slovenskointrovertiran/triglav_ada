9bee5f51e45973df8bffa001ea71a246fe1785a7
Melvil <hbmelvil@gmail.com>
2026-08-09 18:03:40 +0200

    daily ada exercice : using a package

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Week;
--- References the Week package and adds a dependency from Main to Week
 
 procedure Main is
+	use Week; -- Makes every entity of the week package directly visible here.
 begin
-	Put_Line ("First day of the week is " & Week.Mon);
+	Put_Line ("First day of the week is" & Mon);
 end Main;
