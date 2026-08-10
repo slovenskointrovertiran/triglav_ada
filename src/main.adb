78947ab5cd5de5f7eb3ab6d0dbdb56ff4bc1c40e
Melvil <hbmelvil@gmail.com>
2026-08-10 17:54:35 +0200

    daily ada exercice : package child

--- src/main.adb
+++ src/main.adb
@@ -1,31 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Operations;
+with Week.Child; use Week.Child;
 
 procedure Main is
-	use Operations;
-
-	I : Integer := 0;
-	R : Integer;
-
-	procedure Display_Update_Values is
-		Incr : constant Integer := Get_Increment_Value;
-	begin
-		Put_Line (Integer'Image (I) & "incremented by " & Integer'Image (Incr) & " is " & Integer'Image (R));
-		I := R;
-	end Display_Update_Values;
 begin
-	R := Increment_By (I);
-	Display_Update_Values;
-	R := Increment_By (I);
-	Display_Update_Values;
-
-	R := Increment_By (I, 5);
-	Display_Update_Values;
-	R := Increment_By (I);
-	Display_Update_Values;
-
-	R := Increment_By (I, 10);
-	Display_Update_Values;
-	R := Increment_By (I);
-	Display_Update_Values;
+	Put_Line ("First day of the week is " & Get_First_Of_Week);
 end Main;
---
+++ src/week.ads
@@ -0,0 +1,10 @@
+package Week is
+
+	Mon : constant String := "Monday";
+	Tue : constant String := "Tuesday";
+	Wed : constant String := "Wednesday";
+	Thu : constant String := "Thursday";
+	Sat : constant String := "Saturday";
+	Sun : constant String := "Sunday";
+
+end Week;
