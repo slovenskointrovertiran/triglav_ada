14e6747f60449fd4ff06849aefc7d43c022dbc4e
Melvil <hbmelvil@gmail.com>
2026-08-12 22:09:00 +0200

    daily ada exercice : package visibility solution

--- src/book-additional_operations.adb
+++ src/book-additional_operations.adb
@@ -2,14 +2,12 @@ package body Book.Additional_Operations is
 
 	function Get_Extended_Title return String is
 	begin
-		return "Book Title: " & Title;
+		return "Book Title : " & Get_Title;
 	end Get_Extended_Title;
 
 	function Get_Extended_Author return String is
 	begin
-		-- "Author" string declared in Book package body, not visible here.
-		
-		return "Book Author: Unknown"; 
+		return "Book Author: " & Get_Author;
 	end Get_Extended_Author;
 
 end Book.Additional_Operations;
