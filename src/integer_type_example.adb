e57ad2bad4f974799930b1ae08138d598da67c84
Melvil <hbmelvil@gmail.com>
2026-08-14 11:14:35 +0200

    daily ada exercice : operational semantics (overflow)

--- src/integer_type_example.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Integer_Type_Example is
-	type My_Int is range -1 .. 20;
-begin
-	for I in My_Int loop
-		Put_Line (My_Int'Image (I));
-	end loop;
-end Integer_Type_Example;
---
+++ src/main.adb
@@ -0,0 +1,6 @@
+procedure Main is
+	A : Integer := Integer'Last;
+	B : Integer;
+begin
+	B := A + 5; -- This operation will raise an overflow !
+end Main;
