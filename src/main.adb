4f82f9c6a446a6147b27cb83e65ae5d9d31f7444
Melvil <hbmelvil@gmail.com>
2026-08-14 11:34:14 +0200

    daily ada exercice : operational semantics (2/2)

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,15 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 procedure Main is
-	A : Integer := Integer'Last;
-	B : Integer;
+	type My_Int is range 1 .. 20;
+	A : My_Int := 12;
+	B : My_Int := 15;
+	M : My_Int := (A + B) / 2; -- No overflow here
+
 begin
-	B := A + 5; -- This operation will raise an overflow !
+	for I in 1 .. M loop
+		Put_Line ("Hello, World !");
+	end loop;
+
 end Main;
+
