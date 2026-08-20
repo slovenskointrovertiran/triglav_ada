9e4c49fbb03e4574ca44cf415821a6470b556b2f
Melvil <hbmelvil@gmail.com>
2026-08-20 20:51:12 +0200

    daily ada exercice : strong typing, different types of the same family incompatible with each other

--- src/custom_range_types.adb
+++ src/custom_range_types.adb
@@ -1,8 +1,16 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Numerics; use Ada.Numerics;
 
-procedure Custom_Range_Types is
-	type T6_Inv_Trig is digits 6 range -Pi / 2.0 .. Pi / 2.0;
+procedure Illegal_Example is
+	-- Two different floating point types
+	type Meters is new Float;
+	type Miles is new Float;
+
+	Dist_Imperial : Miles;
+
+	-- Declare a constant
+	Dist_Metric : constant Meters := 1000.0;
 begin
-	null;
-end Custom_Range_Types;
+	-- Type mismatch !
+	Dist_Imperial := Dist_Metric * 621.371e-6;
+	Put_Line (Miles'Image (Dist_Imperial));
+end Illegal_Example;
