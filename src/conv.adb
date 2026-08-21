0b07a84584b21b0021bb3a4787d745533d101902
Melvil <hbmelvil@gmail.com>
2026-08-21 17:06:43 +0200

    daily ada exercice : strong typing, types conversionfunction

--- src/conv.adb
+++ src/conv.adb
@@ -2,9 +2,15 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Conv is
 	type Meters is new Float;
 	type Miles is new Float;
+
+	function To_Miles (M : Meters) return Miles is 
+	begin
+		return Miles (M) * 621.371e-6;
+	end To_Miles;
+
 	Dist_Imperial : Miles;
 	Dist_Metric : constant Meters := 1000.0;
 begin
-	Dist_Imperial := Miles (Dist_Metric) * 621.371e-6;
+	Dist_Imperial := To_Miles (Dist_Metric);
 	Put_Line (Miles'Image (Dist_Imperial));
 end Conv;
