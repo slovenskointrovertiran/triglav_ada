2fdafc2ae71eacfde0eaaf0fe9e05fe07dd03a68
Melvil <hbmelvil@gmail.com>
2026-08-21 11:25:51 +0200

    daily ada exercice : strong typing, types conversion

--- src/conv.adb
+++ src/conv.adb
@@ -1,16 +1,10 @@
 with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Illegal_Example is
-	-- Two different floating point types
+procedure Conv is
 	type Meters is new Float;
 	type Miles is new Float;
-
 	Dist_Imperial : Miles;
-
-	-- Declare a constant
 	Dist_Metric : constant Meters := 1000.0;
 begin
-	-- Type mismatch !
-	Dist_Imperial := Dist_Metric * 621.371e-6;
+	Dist_Imperial := Miles (Dist_Metric) * 621.371e-6;
 	Put_Line (Miles'Image (Dist_Imperial));
-end Illegal_Example;
+end Conv;
