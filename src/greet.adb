72db66fd8c091db75c33d6253e5da9918f8ebd53
Melvil <hbmelvil@gmail.com>
2026-08-23 15:44:53 +0200

    daily ada exercice : subtypes

--- src/greet.adb
+++ src/greet.adb
@@ -3,8 +3,19 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Greet is
 	type Days is (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday);
 
-	type Weekend_Days is new Days range Saturday .. Sunday;
+	subtype Weekend_Days is Days range Saturday .. Sunday;
+
+	M : Days := Sunday;
+
+	S : Weekend_Days := M;
 
 begin
-	null;
+	for I in Days loop
+		case I is
+			when Weekend_Days =>
+				Put_Line ("Week end !");
+			when others =>
+				Put_Line ("Hello on " & Days'Image (I));
+		end case;
+	end loop;
 end Greet;
