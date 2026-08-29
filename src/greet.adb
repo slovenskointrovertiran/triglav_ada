3c271fce6daba186ae817b4d32e918404690853c
Melvil <hbmelvil@gmail.com>
2026-08-29 20:14:47 +0200

    daily ada exercice : arrays bounds check

--- src/greet.adb
+++ src/greet.adb
@@ -1,17 +1,16 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Greet is
-	type My_Int is range 1 .. 5;
-	type Your_Index is range 1 .. 5;
+	type My_Int is range 0 .. 1000;
+	type Index is range 1 .. 5;
 
-	type My_Int_Array is
-		array (My_Index) of My_Int;
+	type My_Int_Array is array (Index) of My_Int;
 
-		Tab : My_Int_Array := (2, 3, 5, 7, 11);
-begin
-	for I in Your_Index loop
+	Tab : My_Int_Array := (2, 3, 5, 7, 11);
+
+begin 
+	for I in Index range 2 .. 6 loop
 		Put (My_Int'Image (Tab (I)));
-		-- Compile time error !
 	end loop;
 	New_Line;
 end Greet;
