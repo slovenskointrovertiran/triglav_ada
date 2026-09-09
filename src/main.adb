39a098e6674e1739439a24022c13ee5b92a7d841
Melvil <hbmelvil@gmail.com>
2026-09-09 19:05:06 +0200

    daily ada exercice : overloading ambiguity

--- src/main.adb
+++ src/main.adb
@@ -3,6 +3,12 @@ with Pkg; use Pkg;
 
 procedure Main is
 	S : String := Convert (123_145_299);
+
+	S2 : String := Convert (SSID'(123_145_299));
+
+	I : SSID := 123_145_299;
+
+	S3 : String := Convert (I);
 begin
 	Put_Line (S);
 end Main;
--- src/pkg.ads
+++ src/pkg.ads
@@ -5,4 +5,6 @@ package Pkg is
 
 	function Convert (Self : SSID) return String;
 
+	function Convert (Self : Integer) return String;
+
 end Pkg;
