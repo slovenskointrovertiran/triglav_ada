eea1095cce7540e52928804af708fbabbb105f0c
Melvil <hbmelvil@gmail.com>
2026-09-26 22:04:36 +0200

    daily ada exercice : decimal delta (2)

--- src/decimal_fixed_point_types.adb
+++ src/decimal_fixed_point_types.adb
@@ -1,8 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Decimal_Fixed_Point_Types is
-	type Decimal is delta 10.0 ** (-1) digits 3;
-
+	type Decimal is delta 10.0 ** (0) digits 3;
 begin
-	Put_Line ("The decimal precision of Decimal is " & Decimal'Delta'Image);
+	Put_Line ("The minimum value of Decimal is " & Decimal'First'Image);
+	Put_Line ("The maximum value of Decimal is " & Decimal'Last'Image);
 end Decimal_Fixed_Point_Types;
